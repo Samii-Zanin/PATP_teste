2c8f7187fe81020c653a741d6dcced97afd150f6
Samii_Zanin <samiizanin@gmail.com>
2025-02-21 00:12:34 -0300

    COMMIT 2

--- Teste_git.java
+++ Teste_git.java
@@ -3,7 +3,9 @@ package teste;
 public class Teste_git {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("SAISAMSAJDISADIADA");
+		int variavel = 1;
+		System.out.println(variavel);
 
 	}
 
